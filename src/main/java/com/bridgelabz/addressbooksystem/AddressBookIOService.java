7fe36fa73ace4c5222b8ec8511b1711fb0525027
Rushiraj11 <grushiraj95@gmail.com>
2021-10-05 17:03:55 +0530

    Added: Welcome message

---
+++ src/main/java/com/bridgelabz/addressbooksystem/AddressBookIOService.java
@@ -0,0 +1,12 @@
+package com.bridgelabz.addressbooksystem;
+
+
+public class AddressBookIOService {
+    public static void main(String[] args) {
+        System.out.println("Welcome to AddressBook Program");
+    }
+
+}
+
+
+
