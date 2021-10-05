3db246eb8a737b705e54e8d0c4906c67842ff0b1
Rushiraj11 <grushiraj95@gmail.com>
2021-10-05 18:00:59 +0530

    Added:Write and Read contact as  CSV file

--- src/test/java/com/bridgelabz/addressbooksystem/AddressBookServiceTest.java
+++ src/test/java/com/bridgelabz/addressbooksystem/AddressBookServiceTest.java
@@ -1,7 +1,9 @@
 package com.bridgelabz.addressbooksystem;
-
-import java.io.File;
+import java.io.IOException;
 import java.util.Arrays;
+
+import com.opencsv.exceptions.CsvDataTypeMismatchException;
+import com.opencsv.exceptions.CsvRequiredFieldEmptyException;
 import org.junit.jupiter.api.Assertions;
 import org.junit.Test;
 
@@ -18,5 +20,16 @@ public class AddressBookServiceTest {
         long entries = addressBookService.countDataEntries();
         Assertions.assertEquals(1, entries);
 
+    }
+    @Test
+    public void CSV_Test() throws CsvRequiredFieldEmptyException, CsvDataTypeMismatchException, IOException {
+        AddressBook[] arrayOfPer = {
+                new AddressBook("Tanmay", "Kansal", "Chandi Mandir", "Hapur", "UP", "9874568547" , "xyz@gmail.com", 245101),
+                new AddressBook("Prasad", "Kale", "Kannauj", "Kanpur", "UP", "98754788777" , "xyabxt@gmail.com", 245524)
+        };
+        AddressBookCSVFile addressBookCsvReader = new AddressBookCSVFile();
+        addressBookCsvReader.writeDataInCSVFile(Arrays.asList(arrayOfPer));
+        int count = addressBookCsvReader.readData();
+        Assertions.assertEquals(3,count);
     }
 }
